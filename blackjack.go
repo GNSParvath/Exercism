daa417c3bab02f6cda586769ecf325d69da4af8e
GNSParvath <93996039+GNSParvath@users.noreply.github.com>
2021-12-06 14:34:48 +0530

    Add files via upload

---
+++ blackjack.go
@@ -0,0 +1,70 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch {
+	case card == "ace":
+		return 11
+	case card == "two":
+		return 2
+	case card == "three":
+		return 3
+	case card == "four":
+		return 4
+	case card == "five":
+		return 5
+	case card == "six":
+		return 6
+	case card == "seven":
+		return 7
+	case card == "eight":
+		return 8
+	case card == "nine":
+		return 9
+	case card == "ten" || card == "jack" || card == "queen" || card == "king":
+		return 10
+	default:
+		return 0
+	}
+
+}
+
+// IsBlackjack returns true if the player has a blackjack, false otherwise.
+func IsBlackjack(card1, card2 string) bool {
+	return ParseCard(card1)+ParseCard(card2) == 21
+}
+
+// LargeHand implements the decision tree for hand scores larger than 20 points.
+func LargeHand(isBlackjack bool, dealerScore int) string {
+	switch isBlackjack {
+	case true:
+		switch dealerScore {
+		case 10, 11:
+			return "S"
+		default:
+			return "W"
+		}
+	case false:
+		switch dealerScore {
+		case 11:
+			return "P"
+		default:
+			return "S"
+		}
+	}
+	return "S"
+}
+
+// SmallHand implements the decision tree for hand scores with less than 21 points.
+func SmallHand(handScore, dealerScore int) string {
+	if handScore >= 17 {
+		return "S"
+	} else if handScore <= 11 {
+		return "H"
+	} else if handScore >= 12 && handScore <= 16 && dealerScore <= 6 {
+		return "S"
+	} else if handScore >= 12 && handScore <= 16 && dealerScore >= 7 {
+		return "H"
+	}
+	return "S"
+}
